61b20bfe4edb215c3a7c9d608494effaa00290c7
goinnn <goinnn@9df5bbc6-2602-82a0-fa3b-af6e0d1beed8>
2010-08-31 12:54:27 +0000

    A little error for a change of application name
    git-svn-id: http://django-smart-extends.googlecode.com/svn/trunk@5 9df5bbc6-2602-82a0-fa3b-af6e0d1beed8

--- smartextends/__init__.py
+++ smartextends/__init__.py
@@ -1,4 +1,4 @@
 from django.template.loader import add_to_builtins
 
 
-add_to_builtins('templateextends.templatetags.smart_extends_tags')
+add_to_builtins('smartextends.templatetags.smart_extends_tags')
