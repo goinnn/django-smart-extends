377ac7080f4caa24aaa385a54b3b39c954005c4c
goinnn <goinnn@9df5bbc6-2602-82a0-fa3b-af6e0d1beed8>
2011-12-03 12:33:09 +0000

    Add the readme and change the setup
    git-svn-id: http://django-smart-extends.googlecode.com/svn/trunk@26 9df5bbc6-2602-82a0-fa3b-af6e0d1beed8

---
+++ setup.py
@@ -0,0 +1,79 @@
+# Copyright (c) 2010 by Yaco Sistemas <pmartin@yaco.es>
+#
+# This software is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import re
+
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
+def parse_requirements(file_name):
+    requirements = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'(\s*#)|(\s*$)', line):
+            continue
+        if re.match(r'\s*-e\s+', line):
+            requirements.append(re.sub(r'\s*-e\s+.*#egg=(.*)$', r'\1', line))
+        elif re.match(r'\s*-f\s+', line):
+            pass
+        else:
+            requirements.append(line)
+
+    return requirements
+
+
+def parse_dependency_links(file_name):
+    dependency_links = []
+    for line in open(file_name, 'r').read().split('\n'):
+        if re.match(r'\s*-[ef]\s+', line):
+            dependency_links.append(re.sub(r'\s*-[ef]\s+', '', line))
+
+    return dependency_links
+
+
+data_files = []
+for dirpath, dirnames, filenames in os.walk('.'):
+    for i, dirname in enumerate(dirnames):
+        if dirname.startswith('.'):
+            del dirnames[i]
+    if '__init__.py' in filenames:
+        continue
+    elif filenames:
+        data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
+
+
+setup(
+    name="django-smart-extends",
+    version="0.5",
+    author="Yaco Sistemas S.L.",
+    author_email="pmartin@yaco.es",
+    description="This application is useful when you want to overwrite a template of a application in your project. Currently this in Django produce infinite recursion ",
+    long_description=(read('README')),
+    url='http://code.google.com/p/django-smart-extends/',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    ],
+    license="LGPL 3",
+    keywords="django extends templatetags infinite recursion",
+    packages=find_packages('.'),
+    include_package_data=True,
+    zip_safe=False,
+)
