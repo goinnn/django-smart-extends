6e27d0311f54e5c7b22f3ddb0e9b029c133d84e0
Pablo Martín <goinnn@gmail.com>
2013-09-30 20:58:10 +0200

    Travis test

--- example/example/app/tests.py
+++ example/example/app/tests.py
@@ -43,6 +43,9 @@ class SmartExtendsCase(TestCase):
         self.assertEqual(str_extension in response.content.decode('utf-8'), True)
 
     def test_smart_extends_add_form(self):
+        import sys
+        if sys.version_info[0] == 3:
+            x = 3 / 0
         client = self.__client_login()
         self.check_url(client, reverse('admin:auth_group_add'))
 
